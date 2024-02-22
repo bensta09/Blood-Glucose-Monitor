56775c7921c58fa236e1f24d6ea3f7eedef33492
Benjamin Smith <bensta099@gmail.com>
2024-02-22 16:51:58 -0600

    Added more input fields
    Added carbs, insulin, notes as columns in the database. Also added more input fields as textboxes.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,6 +18,16 @@ using System.Data;
 using System.Windows.Forms.DataVisualization; //for graphing
 
 
+
+/*        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Glucose INTEGER,
+        Carbs INTEGER,
+        Insulin INTEGER,
+        Notes TEXT,
+        Date TEXT,
+        Time TEXT */
+
+
 namespace Blood_Glucose_Monitor
 {
     /// <summary>
@@ -46,7 +56,7 @@ namespace Blood_Glucose_Monitor
             string insert = "Insert into records"
         }
         */
-        private void LoadDataBase() //Method to load database with error handling. Crashes without catch.
+private void LoadDataBase() //Method to load database with error handling. Crashes without catch.
         {
             try
             {
@@ -59,8 +69,8 @@ namespace Blood_Glucose_Monitor
                 DataTable dt = new DataTable();
                 dataAdapter.Fill(dt);
 
-                GGrid.ItemsSource = dt.DefaultView;
-                GGrid_Copy.ItemsSource = dt.DefaultView;
+                //Chart_Grid.ItemsSource = dt.DefaultView;
+                Log_Grid.ItemsSource = dt.DefaultView;
 
                 conn.Close();
             }
@@ -78,7 +88,11 @@ namespace Blood_Glucose_Monitor
 
         private void Entry_Click(object sender, RoutedEventArgs e) //Blood glucose logger
         {
-            String entry = Glucose_Entry.Text;
+            String glucoseEntry = Glucose_Entry.Text;
+            String carbsEntry = Carbs_Entry.Text;
+            String insulinEntry = Insulin_Entry.Text;
+            String notesEntry = Notes_Entry.Text;
+
             DateTime currentDate = DateTime.Now;
             String date = currentDate.ToString("yyyy-MM-dd");
             String time = currentDate.ToString("t");
@@ -89,11 +103,14 @@ namespace Blood_Glucose_Monitor
                 dbPath = Directory.GetCurrentDirectory() + "\\glucose_database.db";
                 SQLiteConnection conn = new SQLiteConnection($"Data Source={dbPath};");
                 conn.Open();
-                string query = "Insert into records (Glucose, Date, Time) values (@entry, @date, @time)";//Glucose, Date, Time
+                string query = "Insert into records (Glucose, Carbs, Insulin, Notes, Date, Time) values (@glucoseEntry, @carbsEntry, @insulinEntry, @notesEntry, @date, @time)";//Glucose, Date, Time
 
                 using (SQLiteCommand command = new SQLiteCommand(query, conn))
                 {
-                    command.Parameters.AddWithValue("@entry", entry);
+                    command.Parameters.AddWithValue("@glucoseEntry", glucoseEntry);
+                    command.Parameters.AddWithValue("@carbsEntry", carbsEntry);
+                    command.Parameters.AddWithValue("@insulinEntry", insulinEntry);
+                    command.Parameters.AddWithValue("@notesEntry", notesEntry);
                     command.Parameters.AddWithValue("@date", date);
                     command.Parameters.AddWithValue("@time", time);
                     command.ExecuteNonQuery();
@@ -107,6 +124,40 @@ namespace Blood_Glucose_Monitor
             }
 
             LoadDataBase();
+        }
+
+        private void btnQuery_Click_1(object sender, RoutedEventArgs e)
+        {
+            var dateFr = dateFrom.SelectedDate;
+            var dateT = dateTo.SelectedDate;
+
+            LoadDataBase();
+
+            try
+            {
+                dbPath = Directory.GetCurrentDirectory() + "\\glucose_database.db";
+                SQLiteConnection conn = new SQLiteConnection($"Data Source={dbPath};");
+                conn.Open();
+                string query = "select * from records where Date between @dateFr and @dateT";//Glucose, Date, Time
+
+                using (SQLiteCommand command = new SQLiteCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@dateFr", dateFr.Value.ToString("yyyy-MM-dd"));
+                    command.Parameters.AddWithValue("@dateT", dateT.Value.ToString("yyyy-MM-dd"));
+                    command.ExecuteNonQuery();
+
+                    SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+                    DataTable dt = new DataTable();
+                    dataAdapter.Fill(dt);
+                    Chart_Grid.ItemsSource = dt.DefaultView;
+                }
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show("An error has occured");
+            }
         }
     }
 }
